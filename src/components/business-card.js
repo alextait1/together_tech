a138e99234885e26a6c04a9e91e9065638bc0c65
Alex Tait <alexandratait@gmail.com>
2020-04-05 10:38:18 -0400

    adding a couple of businesses

--- src/components/business-card.js
+++ src/components/business-card.js
@@ -6,6 +6,7 @@ export const BusinessCard = (props) => {
         <a href={props.href}>
             <div className="business-card-header">
                 <h3>{props.name}</h3>
+                <a className="website-link" href={props.href}>Visit Website</a>
                 <img className="business-card-image"alt={props.alt} src={props.src}/>    
             </div>
             <div className="business-card-body">
@@ -13,10 +14,9 @@ export const BusinessCard = (props) => {
                     <h4>Goods/Services: </h4>
                     <p>{props.goods}</p>
                 </div>
-                <a className="website-link" href={props.href}>Visit Website</a>
                 <div className="pickup-delivery">
-                    <h4 className="mr-20">Delivery? {props.delivery ? 'Yes' : 'No' }</h4>
-                    <h4>Pick up? {props.pickup ? 'Yes' : 'No' }</h4>
+                    <h4 className="mr-20">Delivery? {props.delivery ? <span className="yes">Yes</span> : <span className="no">No</span> }</h4>
+                    <h4>Pick up? {props.pickup ? <span className="yes">Yes</span> : <span className="no">No</span> }</h4>
                 </div>
                 <p>{props.details}</p>
                 <div className="neighbourhood-container">
